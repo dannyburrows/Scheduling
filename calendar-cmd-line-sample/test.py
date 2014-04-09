fb005bd50aa30bdb9c620ef66d71a80ae0f0320d
Danny Burrows <burrows.danny@gmail.com>
2014-04-09 00:14:57 -0500

    cleaning up file structure

--- calendar-cmd-line-sample/test.py
+++
@@ -1,9 +0,0 @@
-from calendarAPI import person
-
-user = person("burrows.danny@gmail.com")
-user.findAvailbility("04/10/2014 08:30", "04/10/2014 15:00")
-user.listAvailabilities()  
-
-newUser = person("jjames83@gmail.com")
-newUser.findAvailbility("04/10/2014 08:30", "04/10/2014 15:00")
-newUser.listAvailabilities()  
